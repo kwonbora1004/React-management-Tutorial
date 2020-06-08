ce9ced472df98f71445ce8367bad046fe30e1024
kwonbora1004 <candy9837@gmail.com>
2020-06-08 22:46:11 +0900

    re table

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Table from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 
@@ -10,7 +9,7 @@ class Customer extends React.Component{
         <div>
             <TableRow>
                 <TableCell>{this.props.id}</TableCell>
-                <TableCell><img src={this.props.image} alt="prohile"/></TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell>{this.props.birthday}</TableCell>
                 <TableCell>{this.props.gender}</TableCell>
@@ -21,27 +20,4 @@ class Customer extends React.Component{
     }
 }
 
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-            <p>{this.props.birthday}</p>
-            <p>{this.props.gender}</p>
-            <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 export default Customer;
